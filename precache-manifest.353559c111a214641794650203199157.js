831e69a9d98bc53a212c5cb924e5838d605e5d7e
EldadB <Eladb@playbuzz.vom>
2018-11-09 10:26:23 +0200

    Updates

---
+++ precache-manifest.353559c111a214641794650203199157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26e27f3d1bc6b06fb292",
+    "url": "/driver/static/js/runtime~main.26e27f3d.js"
+  },
+  {
+    "revision": "5ef6be68cb9c0ec8a260",
+    "url": "/driver/static/js/main.5ef6be68.chunk.js"
+  },
+  {
+    "revision": "b422a5c7522e53422059",
+    "url": "/driver/static/js/1.b422a5c7.chunk.js"
+  },
+  {
+    "revision": "5ef6be68cb9c0ec8a260",
+    "url": "/driver/static/css/main.ca4b5d92.chunk.css"
+  },
+  {
+    "revision": "b422a5c7522e53422059",
+    "url": "/driver/static/css/1.f9d3fa1e.chunk.css"
+  },
+  {
+    "revision": "c9f64dd20d2824232a35f22ceeae21d7",
+    "url": "/driver/index.html"
+  }
+];
